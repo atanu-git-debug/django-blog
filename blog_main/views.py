c72ba178728335625960d7eb4dbe2e2a1d4d7da4
atanu-git-dubug <imatanu0@gmail.com>
2026-01-21 22:07:48 +0530

    made the home page dynamic

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,7 +1,16 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from blogs.models import Category,Blog
 
 
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True,status='published').order_by('updated_at')
+    posts = Blog.objects.filter(is_featured=False,status='published').order_by('updated_at')
+    context = {
+        'categories': categories,
+        'featured_posts': featured_posts,
+        'posts': posts,
+    }
+    return render(request, 'home.html',context)
