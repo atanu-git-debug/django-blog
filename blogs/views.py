72ae4633e16b365780a9c42bb9df1dc67272d947
atanu-git-dubug <imatanu0@gmail.com>
2026-01-22 22:23:19 +0530

    made the category section

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from unicodedata import category
+from django.shortcuts import get_object_or_404, render,redirect
+from django.http import HttpResponse
+
+from blogs.models import Blog, Category
 
 # Create your views here.
+def post_by_category(request, pk):
+    posts = Blog.objects.filter(category=pk, status='published')
+   # try:
+    #    category = Category.objects.get(pk=pk)
+    #except Category.DoesNotExist:
+     #   return redirect('home')
+    category = get_object_or_404(Category, pk=pk)   
+    context = {
+        'posts': posts,
+        'category_name': category.category_name
+    }
+    return render(request, 'category_detail.html', context)
