729658cd9c6c9107fc178d4ccc55f6426ba071ac
atanu-git-dubug <imatanu0@gmail.com>
2026-01-28 10:32:31 +0530

    category curd functiolily

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,4 +6,9 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('categories/',views.categories,name='categories'),
+    path('categories/add',views.add_category,name='add_category'),
+    path('categories/edit/<int:id>/',views.edit_category,name='edit_category'),
+    path('categories/delete/<int:id>/',views.delete_category,name='delete_category'),
+
 ]
